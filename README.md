79378233e09ef378878d49dadc19a01eeff26f00
importcjj <importcjj@gmail.com>
2019-10-09 01:33:50 +0800

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-首先 需要rust的nightly版本
+## 开发准备
+
+Rust环境: nightly
+
+异步准备
+
+[async-std](https://github.com/async-rs/async-std)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use tokio::net::TcpListener;
+use tokio::io::AsyncReadExt;
+use tokio::runtime::Runtime;
+
+async fn run_server() -> Result<(), Box<dyn std::error::Error>> {
+    let mut listener = TcpListener::bind("127.0.0.1:7788").await?;
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        
+        tokio::spawn(async move {
+            let (mut reader, mut writer) = socket.split();
+            reader.copy(&mut writer).await.unwrap();
+        });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rt = Runtime::new().unwrap();
+    rt.block_on(run_server());
 }
